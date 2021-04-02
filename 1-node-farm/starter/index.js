40aef5e426b0761b4c8e609f558b1ceef0801090
BROKY <catur@windowslive.com>
2021-04-02 20:09:46 +0700

    readFile

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
 
-fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-});
+const textIn = fs.readFile('./txt/input.txt', 'utf-8');
+console.log(textIn);
